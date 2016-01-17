001ce08188dd06634134728dcf7ddd8fd9aa1012
atostudios <https://atostudios@github.com>
2016-01-17 16:58:00 +0000

    added layout module

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
+	browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+	return gulp.src('./scss/**/*')
+	.pipe(sass.sync().on('error', sass.logError))
+	.pipe(sass({includePaths: ['./scss']}))
+	.pipe(gulp.dest('./css'));
+});
+
+gulp.task('autoprefix', ['sass'], function() {
+	return gulp.src('./css/styles.css')
+	.pipe(autoprefixer())
+	.pipe(gulp.dest('./css/'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+      server: {
+        baseDir: "./"
+      }
+    });
+    gulp.watch(["./css/*.css", "./index.html"]).on("change", browserSync.reload);
+  });
+
+gulp.task('watch-files', function() {
+	gulp.watch('./scss/**/*', ['autoprefix']);
+});
+
+gulp.task('default', ['autoprefix', 'watch-files', 'browser-sync']);
